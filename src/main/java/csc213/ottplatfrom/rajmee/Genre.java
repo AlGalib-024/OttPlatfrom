f8c2ed580b2d0fdfe193d3f12a475dfd1abc826f
rajmee01 <2521739@iub.edu.bd>
2026-07-25 15:51:25 +0600

    class completed

---
+++ src/main/java/csc213/ottplatfrom/rajmee/Genre.java
@@ -0,0 +1,10 @@
+package csc213.ottplatfrom.rajmee;
+
+public class Genre {
+    private int genreId;
+    private String genreName;
+
+    public boolean assignGenre(int contentId) {
+        return true;
+    }
+}
